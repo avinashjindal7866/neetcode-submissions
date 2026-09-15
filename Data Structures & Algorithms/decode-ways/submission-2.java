d7c5363af1c18c0d4dfe4afa694b5d787423c44c
avinashjindal7866 <159506527+avinashjindal7866@users.noreply.github.com>
2026-09-15 19:38:20 +0530

    Add: decode-ways - submission-2

---
+++ Data Structures & Algorithms/decode-ways/submission-2.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int numDecodings(String s) {
+        Map<Integer, Character> map = new HashMap<>();
+        Map<Integer, Integer> dp = new HashMap<>();
+        for (int i = 1; i <= 26; i++) {
+            map.put(i, (char) ('A' + i - 1));
+        }
+
+        
+
+        return recursion(map,s,0,dp);
+    }
+
+    public static int recursion(Map<Integer, Character> map,String s,int i,Map<Integer, Integer> dp){
+        if (s.length() == i) {
+        return 1;
+    }
+
+    if(dp.containsKey(i)){
+        return dp.get(i);
+    }
+
+    int one = 0;
+    int two = 0;
+
+    if (map.containsKey(Integer.parseInt(s.substring(i, i + 1))) && s.charAt(i) != '0') {
+        one = recursion(map, s, i + 1,dp);
+    }
+
+    if (i + 2 <= s.length() && s.charAt(i) != '0'
+            && map.containsKey(Integer.parseInt(s.substring(i, i + 2)))) {
+        two = recursion(map, s, i + 2,dp);
+    }
+
+    dp.put(i,one + two);
+
+    return dp.get(i);
+    }
+}
